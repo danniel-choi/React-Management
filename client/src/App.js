5b2278971a86048c7e6b9c165dff646838b2dbc8
DannielChoi <danniel.choi@gmail.com>
2020-12-31 15:32:07 +0900

    package modify setting

--- client/src/App.js
+++ client/src/App.js
@@ -155,12 +155,7 @@ class App extends Component {
       <div className={classes.root}>
         <AppBar position="static">
           <Toolbar>
-            <IconButton
-              edge="start"
-              className={classes.menuButton}
-              color="inherit"
-              aria-label="open drawer"
-            >
+            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="open drawer" >
               <MenuIcon />
             </IconButton>
             <Typography className={classes.title} variant="h6" noWrap>
